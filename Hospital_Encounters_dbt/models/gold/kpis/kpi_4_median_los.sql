4380dc479297c44982369df971c5d5f372d08f28
Verulam Blue <verulam.blue@gmail.com>
2026-01-19 20:48:53 +0000

    Updated kpi 4 results

--- Hospital_Encounters_dbt/models/gold/kpis/kpi_4_median_los.sql
+++ Hospital_Encounters_dbt/models/gold/kpis/kpi_4_median_los.sql
@@ -1,8 +1,16 @@
-
- 
-select
-  'kpi_3' as kpi_name,
-  cast(round(avg(cast(length_of_stay_days as float)), 3) as varchar(50)) as kpi_value,
+with base as (
+  select cast(length_of_stay_days as float) as los
+  from {{ ref('gold_encounters_final') }}
+  where is_admitted = 1 and los_eligible_flag = 1
+),
+med as (
+  select
+    percentile_cont(0.5) within group (order by los) over () as median_los
+  from base
+)
+select top 1
+  'kpi_4' as kpi_name,
+  cast(round(median_los, 3) as varchar(50)) as kpi_value,
   cast(null as varchar(200)) as kpi_key
-from {{ ref('gold_encounters_final') }}
-where is_admitted = 1 and los_eligible_flag = 1
+from med
+
